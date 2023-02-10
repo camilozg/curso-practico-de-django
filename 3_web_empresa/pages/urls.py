d90db9b8a9e3ea12341726eb1a0ebcf5df73e840
camilozg <camilozamora1@gmail.com>
2023-02-09 20:39:39 -0500

    Delete 3_web_empresa directory

--- 3_web_empresa/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'pages'
-
-urlpatterns = [
-    path('<int:page_id>/<slug:page_slug>', views.page, name='page')
-]
--- 3_web_empresa/social/processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Link
-
-
-def context(request):
-    context = {}
-    links = Link.objects.all()
-    for link in links:
-        context[link.key] = link.url
-    return context
