96ea3b59a8fd27fbe5ed682e7ba0441218d90a3f
camilozg <camilozamora1@gmail.com>
2023-02-09 20:39:24 -0500

    Delete 2_web_playground directory

--- 2_web_playground/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProfileListView, ProfileDetailView
-
-app_name = 'profiles'
-
-urlpatterns = ([
-    path('', ProfileListView.as_view(), name='list'),
-    path('<username>/', ProfileDetailView.as_view(), name='detail'),
-])
